9bc0124a7845927fc1df445fa5a5e8abf06d1e6d
Domenico <kiuz.4ever@gmail.com>
2009-06-28 22:47:15 +0200

    first script

---
+++ recoursive-ext-detection.php
@@ -0,0 +1,22 @@
+<?php
+//simple example of recursive require and read *.ext of files
+$dir = "./path_file/"; //Set dir
+$dh = opendir($dir);
+ 
+while (($file = readdir($dh)) !== false) {
+ 
+  $fExt= end(explode(".", $file));
+ 
+  if ($fExt=='php'){ //if ext of file is php or other *.ext
+    $rFile[]=$file; //add file to array
+  }
+}
+ 
+closedir($dh);
+ 
+// recursive inlcuding of file of array
+for ($d=0;$d<$dmax=(sizeof($rFile));$d++){
+  require ($dir.$rFile[$d]);  
+}
+ 
+?>
